925f136aa05223629df0aad558625e7c29a760a5
dogurasu <dugs134@gmail.com>
2020-07-24 14:11:16 -0700

    started working on P&L functionality

--- scrubData.py
+++ scrubData.py
@@ -1,9 +1,16 @@
 import pandas, pathlib
 
+# TODO
+# - Implement P&L Calculation after each trade
+#     - keep track of a flag for whether a new trade was started or not(buy or sell of 50 shares)
+#     - keep a counter for how many shares have been covered so far
+#     - reset the counter and flag after calculating total profits or losses
+# - Calculate commissions for each trade (using CMEG's $0.50 minimum)
+
 # get path to file
-# for windows vscode terminal - pandas.read_csv(str(pathlib.Path().parent.absolute()) + "\\trading_data\\7-22-2020\\7-22-2020.csv")
+# for windows vscode terminal - pandas.read_csv(str(pathlib.Path().parent.absolute()) + "\\trading_data\\7-24-2020\\7-24-2020.csv")
 path = str(pathlib.Path(__file__).parent.absolute())
-month, day = '7', '23'
+month, day = '7', '24'
 txt_path = path + f"\\trading_data\\{month}-{day}-2020\\{month}-{day}-2020.csv"
 
 # check out the data
@@ -28,8 +35,36 @@ for arr in reversed(data.values):
         exec_orders['route'].append(arr[5])
         exec_orders['time_filled'].append(arr[6])
 
-# Implement Profit/Loss
+exec_orders = pandas.DataFrame(exec_orders, columns=["order_type", "ticker", "size", "price_filled", "route", "time_filled", "net_profit"])
 
-df = pandas.DataFrame(exec_orders, columns=["order_type", "ticker", "size", "price_filled", "route", "time_filled"])
+# TODO
+# - Implement P&L Calculation after each trade
+#     - keep track of a flag for whether a new trade was started or not(buy or sell of 50 shares)
+#     - keep a counter for how many shares have been covered so far
+#     - reset the counter and flag after calculating total profits or losses
+# - Calculate commissions for each trade (using CMEG's $0.50 minimum)
+
+# Implement Profit/Loss
+completed = {
+    'order_type': [],
+    'ticker': [],
+    'size': [],
+    'price_filled': [],
+    'route': [],
+    'time_filled': [],
+    'net_p&l': [],
+}
+
+new_trade = True
+for order in range(len(exec_orders.values)):
+    # check if we're at a new trade
+    if new_trade:
+        new_trade = False
+        shares_left = 50
+        # set the order type to Buy, Sell or Shrt
+        order_type = exec_orders.values[order][0]
+        # record the total that the trade was initiated (50 shares * the price)
+        total = exec_orders.values[order][3]*50
+        continue 
 
 df.to_csv(path + f"\\trading_data\\{month}-{day}-2020\\{month}-{day}-2020_altered.csv", index=False, header=True)
