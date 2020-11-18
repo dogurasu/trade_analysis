340735c672d11fb22bd6deb4e51f67029bcb5783
dogurasu <dugs134@gmail.com>
2020-11-18 00:41:07 -0800

    added some csv/txt files for practice and renamed AV.py

--- stockscrape/AV.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
-AV_KEY = os.environ.get("AVKEY1")
-print(AV_KEY)
